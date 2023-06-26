91b7b02df096fc5b7eafbc822f33e3a65e0e78cd
57381647+KienPS <57381647+KienPS@users.noreply.github.com>
2023-06-26 21:04:57 +0700

    Create test

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, Depends
+from fastapi.testclient import TestClient
 
 from .routers.auth import register, login
 from .dependencies import get_query_token, get_token_header
@@ -21,3 +22,30 @@ app.include_router(
 @app.get("/")
 async def root():
     return {'msg': 'Welcome to SVN'}
+
+
+client = TestClient(app)
+
+def test_register():
+    response = client.post(
+        url='/api/auth/register',
+        json={
+            'first_name': 'SVN',
+            'last_name': 'logistic',
+            'email': 'svn@gmail.com',
+            'password': 'Svn2023!'
+        }
+    )
+    assert response.status_code == 201
+    assert any(key in response.json().keys() for key in ['email', 'uuid', 'is_active', 'is_verified'])
+
+def test_login():
+    response = client.post(
+        url='/api/auth/login',
+        json={
+            'username': 'svn@gmail.com',
+            'password': 'Svn2023!'
+        }
+    )
+    assert response.status_code == 201
+    assert any(key in response.json().keys() for key in ['access_token', 'token_type'])
