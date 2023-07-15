ec652c650aeb08ac786a94174528cba39727df15
57381647+KienPS <57381647+KienPS@users.noreply.github.com>
2023-07-16 01:07:12 +0700

    Re-configure User Model

--- api/models/user.py
+++ api/models/user.py
@@ -3,32 +3,10 @@ from uuid import uuid4
 
 from django.db import models
 from django.contrib.auth.models import User, UserManager
-
-
-class SvnUserManager(UserManager):
-    def create_user(self, 
-                    first_name: str, 
-                    last_name: str,
-                    email: str, 
-                    password: str | None = ..., **extra_fields: Any) -> Any:
-        uid = uuid4()
-        username: str = "%s %s" % (first_name, last_name).strip()
-        user: SvnUser = self.create(
-            uid=uid,
-            first_name=first_name,
-            last_name=last_name,
-            username=username,
-            email=email,
-        )
-        user.set_password(password)
-        user.save()
-        return user
     
 
 class SvnUser(User):
-    uid = models.CharField(max_length=36, unique=True, primary_key=True, db_index=True)
+    uid = models.UUIDField(primary_key=True, default=uuid4, editable=False)
 
     REQUIRED_FIELDS = ['first_name', 'last_name', 'email']
     USERNAME_FIELD = 'email'
-
-    objects = SvnUserManager
