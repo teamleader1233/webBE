d40b278e88c8464725e531e27f706391522cc058
57381647+KienPS <57381647+KienPS@users.noreply.github.com>
2023-07-23 02:09:57 +0700

    Use 'username' as username_field

--- api/models/user.py
+++ api/models/user.py
@@ -3,16 +3,7 @@ from uuid import uuid4
 
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-from django.utils.translation import gettext as _
     
 
-class SvnUser(AbstractUser):
-    uid = models.UUIDField(primary_key=True, default=uuid4, editable=False)
-    email = models.EmailField(_('email address'), unique=True, blank=False)
-
-    REQUIRED_FIELDS = ['first_name', 'last_name']
-    USERNAME_FIELD = 'email'
-
-    def save(self, force_insert: bool = ..., force_update: bool = ..., using: str | None = ..., update_fields: Iterable[str] | None = ...) -> None:
-        self.username = self.get_full_name()
-        return super().save(force_insert, force_update, using, update_fields)
+class User(AbstractUser):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
