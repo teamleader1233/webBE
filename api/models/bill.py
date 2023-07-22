2afd3d9667a9acc0508d11786cc51b241c419800
57381647+KienPS <57381647+KienPS@users.noreply.github.com>
2023-07-23 02:09:01 +0700

    temporary public api

--- api/models/bill.py
+++ api/models/bill.py
@@ -1,7 +1,8 @@
 from typing import Iterable, Optional
-from django.db import models
-
 from uuid import uuid4
+
+from django.db import models
+from django_bleach.models import BleachField
 
 from ..models.product import Product
 
@@ -13,43 +14,16 @@ STATUS_CHOICES = (
     ('canceled', 'Đã hủy'),
 )
 
-PAYMENT_CHOICES = (
-    ('cod', 'Thanh toán khi nhận hàng'),
-    ('momo', 'Thanh toán qua MoMo'),
-    ('zalopay', 'Thanh toán qua ZaloPay'),
-    ('bank', 'Thanh toán qua ngân hàng'),
-)
 
-
-class AbstractBill(models.Model):
+class Bill(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
-    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='pending')
-    date_created = models.DateTimeField(auto_now=True)
-
-    class Meta:
-        abstract = True
-
-
-class Bill(AbstractBill):
     product = models.ForeignKey(Product, to_field='id', on_delete=models.CASCADE)
-    quantity = models.IntegerField(default=1)
-    address = models.CharField(max_length=100)
-    payment = models.CharField(max_length=10, choices=PAYMENT_CHOICES, default='cod')
-    total = models.IntegerField()
+    quantity = models.PositiveBigIntegerField(default=1)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='pending')
+    address = BleachField(max_length=100)
+    date = models.DateTimeField(auto_now=True)
+    total = models.PositiveBigIntegerField()
     
     def save(self, force_insert: bool = ..., force_update: bool = ..., using: str | None = ..., update_fields: Iterable[str] | None = ...) -> None:
         self.total = self.product.price * self.quantity
-        bill : Bill = super(Bill, self).save(force_insert, force_update, using, update_fields)
-        BillHistory.objects.create(
-            id=id,
-            status=bill.status,
-            date_created=bill.date_created
-        )
-    
-
-class BillHistory(models.Model):
-    bill = models.ForeignKey(Bill, to_field='id', on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_now_add=True)
-    
-    class Meta:
-        ordering = ['-created_at']
+        return super(Bill, self).save(force_insert, force_update, using, update_fields)
--- api/models/product.py
+++ api/models/product.py
@@ -1,11 +1,11 @@
 from django.db import models
+from django_bleach.models import BleachField
 
 
 class Product(models.Model):
-    id = models.CharField(max_length=64, primary_key=True, unique=True)
     name = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=12, decimal_places=2)
-    description = models.TextField()
+    description = BleachField(max_length=512)
     image = models.ImageField(upload_to='products', blank=True)
 
     def __str__(self):
