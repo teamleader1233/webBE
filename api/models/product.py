e369e069d1360e875806464378a899d7b12e39d1
Pham Sy Minh <59408894+shelld3v@users.noreply.github.com>
2023-07-26 20:22:09 +0700

    Prevent possible XSS in name field

--- api/models/product.py
+++ api/models/product.py
@@ -3,7 +3,7 @@ from django_bleach.models import BleachField
 
 
 class Product(models.Model):
-    name = models.CharField(max_length=100)
+    name = BleachField(models.CharField(max_length=100))
     price = models.DecimalField(max_digits=12, decimal_places=2)
     description = BleachField(max_length=512)
 
