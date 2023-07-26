0bf63273981992986752287e15637cb5f8774e60
57381647+KienPS <57381647+KienPS@users.noreply.github.com>
2023-07-26 14:43:53 +0700

    Add blog urls

--- api/urls.py
+++ api/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path, include
 
-from .views import bill, product
+from .views import bill, product, blog
 
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
 router.register('bills', bill.BillViewSet, basename='bill')
 router.register('products', product.ProductViewSet, basename='product')
+router.register('blogs', blog.BlogViewSet, basename='blog')
 
 urlpatterns = [
     
