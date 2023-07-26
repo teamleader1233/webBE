6672aa0e4818ed963800d2d6c4e3050740e34a51
57381647+KienPS <57381647+KienPS@users.noreply.github.com>
2023-07-26 14:45:08 +0700

    Add permission

--- api/utils/permission.py
+++ api/utils/permission.py
@@ -3,6 +3,14 @@ from rest_framework.permissions import BasePermission
 
 class IsAdminUserOrRetrieveOnly(BasePermission):
     def has_permission(self, request, view):
-        if view.action != 'retrieve' and not request.user.is_staff:
+        if view.action != 'retrieve' and (not request.user or not request.user.is_staff):
             return False
         return True
+    
+class IsAdminUserOrReadOnly(BasePermission):
+    SAFE_METHODS = ['GET', 'HEAD', 'OPTIONS']
+
+    def has_permission(self, request, view):
+        if request.method in self.SAFE_METHODS or (request.user and request.user.is_staff):
+            return True
+        return False
