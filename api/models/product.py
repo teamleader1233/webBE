c8bb0f77cf4d21217ac0265e55f63a8ed9daf96e
57381647+KienPS <57381647+KienPS@users.noreply.github.com>
2023-07-26 13:32:27 +0700

    Remove ImageField

--- api/models/product.py
+++ api/models/product.py
@@ -6,7 +6,6 @@ class Product(models.Model):
     name = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=12, decimal_places=2)
     description = BleachField(max_length=512)
-    image = models.ImageField(upload_to='products', blank=True)
 
     def __str__(self):
         return self.name
