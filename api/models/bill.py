7c79c2940d111f0f6d6008085778d2f47badbbb6
57381647+KienPS <57381647+KienPS@users.noreply.github.com>
2023-08-12 10:29:10 +0700

    Update bill model

--- api/models/bill.py
+++ api/models/bill.py
@@ -21,14 +21,17 @@ class Bill(models.Model):
     receiver_name = BleachField(max_length=64)
     receiver_phone = BleachField(max_length=16)
     receiver_address = BleachField(max_length=256)
+    product_image = models.ImageField(blank=True, null=True, default=None)
     product_name = BleachField(max_length=64)
     product_price = models.PositiveBigIntegerField(default=1)
     product_weight = models.DecimalField(max_digits=5, decimal_places=2, default=0)
     product_description = BleachField(max_length=512)
+    precollected_price = models.PositiveBigIntegerField(default=1)
     location = BleachField(max_length=256)
     quantity = models.PositiveBigIntegerField(default=1)
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='pending')
     date = models.DateTimeField(auto_now=True)
+    note = BleachField(max_length=512, null=True, default=None)
     total = models.PositiveBigIntegerField()
 
     class Meta:
