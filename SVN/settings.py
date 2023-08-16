a41f22b58f007bb42b68636c96adacff3c1f6c8a
57381647+KienPS <57381647+KienPS@users.noreply.github.com>
2023-08-16 23:16:16 +0700

    Switching back to authorizing through header

--- SVN/settings.py
+++ SVN/settings.py
@@ -162,7 +162,7 @@ REST_FRAMEWORK = {
         'rest_framework_xml.renderers.XMLRenderer',
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'api.utils.authentication.CookieJWTAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
         'rest_framework.authentication.SessionAuthentication',
     ),
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
